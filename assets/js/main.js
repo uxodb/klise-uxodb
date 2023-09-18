5d5c5c285ff625fe5afdf62efe5a20aae26f88de
uxodb <uxodb@x0rmail.com>
2023-09-18 06:53:27 +0200

    synjs: Enable remembering scrollposition on refresh

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,16 @@
       : area.classList.remove("blurry");
   });
 })();
+
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var scrollpos = sessionStorage.getItem('scrollpos');
+    if (scrollpos) {
+        window.scrollTo(0, scrollpos);
+        sessionStorage.removeItem('scrollpos');
+    }
+});
+window.addEventListener("beforeunload", function (e) {
+    sessionStorage.setItem('scrollpos', window.scrollY);
+});
